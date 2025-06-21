da9c7b355b7ab5c3d65aa456ec690449e3fe4719
ZeeshanZk09 <mzeeshankhan0988@gmail.com>
2025-06-20 21:42:23 -0700

    joined at added and next.config updated

--- src/app/(pages)/profile/page.tsx
+++ src/app/(pages)/profile/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ReactNode, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import Loader from '@/components/ui/Loader';
