014d454ad7530236af7352c97fb7a7b4be2157b0
ZeeshanZk09 <mzeeshankhan0988@gmail.com>
2025-10-01 02:35:10 +0500

    all errors fixed for build

--- src/components/admin/Users.Table.tsx
+++ src/components/admin/Users.Table.tsx
@@ -1,78 +1,66 @@
-"use client";
+'use client';
 
-import { User } from "@/app/lib/models/User";
+import { User } from '@/app/generated/prisma/client/browser';
 
 interface UsersTableProps {
   users: User[];
   onDeleteUser: (userId: string) => void;
-  onToggleAdmin: (userId: string, isAdmin: boolean) => void;
+  onToggleAdmin: (userId: string, role: string) => void;
 }
 
-export default function UsersTable({
-  users,
-  onDeleteUser,
-  onToggleAdmin,
-}: UsersTableProps) {
+export default function UsersTable({ users, onDeleteUser, onToggleAdmin }: UsersTableProps) {
   return (
-    <div className="overflow-x-auto">
-      <table className="min-w-full divide-y divide-gray-200">
-        <thead className="bg-gray-50">
+    <div className='overflow-x-auto'>
+      <table className='min-w-full divide-y divide-gray-200'>
+        <thead className='bg-gray-50'>
           <tr>
             <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+              scope='col'
+              className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
             >
               User
             </th>
             <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+              scope='col'
+              className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
             >
               Email
             </th>
             <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+              scope='col'
+              className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
             >
               Status
             </th>
             <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+              scope='col'
+              className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
             >
               Joined
             </th>
             <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+              scope='col'
+              className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
             >
               Actions
             </th>
           </tr>
         </thead>
-        <tbody className="bg-white divide-y divide-gray-200">
+        <tbody className='bg-white divide-y divide-gray-200'>
           {users.map((user) => (
-            <tr key={user._id.toString()}>
-              <td className="px-6 py-4 whitespace-nowrap">
-                <div className="flex items-center">
-                  <div className="flex-shrink-0 h-10 w-10">
-                    <img
-                      className="h-10 w-10 rounded-full"
-                      src={user.profilePic}
-                      alt=""
-                    />
+            <tr key={user.id}>
+              <td className='px-6 py-4 whitespace-nowrap'>
+                <div className='flex items-center'>
+                  <div className='flex-shrink-0 h-10 w-10'>
+                    <img className='h-10 w-10 rounded-full' src={user.profilePic!} alt='' />
                   </div>
-                  <div className="ml-4">
-                    <div className="text-sm font-medium text-gray-900">
-                      {user.username}
-                    </div>
+                  <div className='ml-4'>
+                    <div className='text-sm font-medium text-gray-900'>{user.username}</div>
                   </div>
                 </div>
               </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {user.email}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap">
+              <td className='px-6 py-4 whitespace-nowrap text-sm text-gray-500'>{user.email}</td>
+              {/* <td className="px-6 py-4 whitespace-nowrap">
                 <span
                   className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${
                     user.isAdmin
@@ -82,26 +70,24 @@ export default function UsersTable({
                 >
                   {user.isAdmin ? "Admin" : "User"}
                 </span>
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+              </td> */}
+              <td className='px-6 py-4 whitespace-nowrap text-sm text-gray-500'>
                 {new Date(user.createdAt).toLocaleDateString()}
               </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
+              <td className='px-6 py-4 whitespace-nowrap text-sm font-medium'>
                 <button
-                  onClick={() =>
-                    onToggleAdmin(user._id.toString(), user.isAdmin)
-                  }
+                  onClick={() => onToggleAdmin(user.id, user.role)}
                   className={`mr-3 ${
-                    user.isAdmin
-                      ? "text-yellow-600 hover:text-yellow-900"
-                      : "text-indigo-600 hover:text-indigo-900"
+                    user.role
+                      ? 'text-yellow-600 hover:text-yellow-900'
+                      : 'text-indigo-600 hover:text-indigo-900'
                   }`}
                 >
-                  {user.isAdmin ? "Remove Admin" : "Make Admin"}
+                  {user.role ? 'Remove Admin' : 'Make Admin'}
                 </button>
                 <button
-                  onClick={() => onDeleteUser(user._id.toString())}
-                  className="text-red-600 hover:text-red-900"
+                  onClick={() => onDeleteUser(user.id.toString())}
+                  className='text-red-600 hover:text-red-900'
                 >
                   Delete
                 </button>
